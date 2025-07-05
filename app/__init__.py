b0354bd067ba417441fb7e84d70bb830df33f83b
Bananbka <bananbka0.2@gmail.com>
2025-07-05 22:26:54 +0300

    init commit

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+﻿from flask import Flask
+
+from app.database import db
+from app.config import Config
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    from app.models.user import User
+    from app.models.partner_preference import PartnerPreference
+    from app.models.meeting import Meeting
+    from app.models.couple import Couple
+    from app.models.refusal import Refusal
+    from app.models.credentials import Credentials
+    with app.app_context():
+        db.create_all()
+
+    return app
---
+++ app/config.py
@@ -0,0 +1,12 @@
+﻿import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = (f"postgresql://"
+                               f"{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}"
+                               f"@localhost/{os.getenv("POSTGRES_DB")}")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
