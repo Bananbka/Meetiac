5f52fda7b3a20de0e221005ae5fbc92dbbb2a513
Bananbka <bananbka0.2@gmail.com>
2025-07-12 18:09:33 +0300

    add serializer User to dict

--- app/models/user.py
+++ app/models/user.py
@@ -25,9 +25,68 @@ class User(db.Model):
                                 cascade="all, delete-orphan")
     refusals = db.relationship('Refusal', backref='user', cascade="all, delete-orphan")
     matchs1 = db.relationship('Match', foreign_keys='Match.user1_id', backref='user1_match',
-                               cascade="all, delete-orphan")
+                              cascade="all, delete-orphan")
     matchs2 = db.relationship('Match', foreign_keys='Match.user2_id', backref='user2_match',
-                               cascade="all, delete-orphan")
+                              cascade="all, delete-orphan")
     interests = db.relationship('Interest', secondary='user_interest', back_populates='users')
     zodiac_sign = db.relationship('ZodiacSign', back_populates='users')
     gender_obj = db.relationship('Gender', backref='users')
+    images = db.relationship('UserImage', backref='user')
+
+    def to_dict(self, fields=None):
+        if fields is None:
+            fields = []
+
+        data = {
+            "user_id": self.user_id,
+            "name": f"{self.first_name} {self.last_name}"
+        }
+
+        if "birth_date" in fields:
+            data["birth_date"] = self.birth_date.isoformat() if self.birth_date else None
+
+        if "age" in fields:
+            data["age"] = self.get_age()
+
+        if "gender" in fields:
+            data["gender"] = self.gender_obj.name if self.gender_obj else None
+
+        if "height" in fields:
+            data["height"] = self.height
+
+        if "weight" in fields:
+            data["weight"] = self.weight
+
+        if "sign" in fields:
+            data["sign"] = self.zodiac_sign.name if self.zodiac_sign else None
+
+        if "bio" in fields:
+            data["bio"] = self.bio
+
+        if "images" in fields:
+            data["images"] = ['static/uploads/user_2_0.png', 'static/uploads/user_2_1.png',
+                              'static/uploads/user_2_2.png']
+            import random
+            random.shuffle(data["images"])
+
+            # data["images"] = [img.image_url for img in self.images]
+
+        if "interests" in fields:
+            data["interests"] = ["Подорожі"]
+            # data["interests"] = [interest.name for interest in self.interests]
+
+        if "is_active" in fields:
+            data["is_active"] = self.is_active
+
+        if "registration_date" in fields:
+            data["registration_date"] = self.registration_date.isoformat() if self.registration_date else None
+
+        return data
+
+    def get_age(self):
+        if self.birth_date:
+            today = date.today()
+            return today.year - self.birth_date.year - (
+                    (today.month, today.day) < (self.birth_date.month, self.birth_date.day)
+            )
+        return None
