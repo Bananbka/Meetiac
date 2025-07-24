e0d42976d048eb4b6e0dfe3eef304ddb7a1e10f9
Bananbka <bananbka0.2@gmail.com>
2025-07-24 13:33:15 +0300

    expand meeting table

--- app/models/meeting.py
+++ app/models/meeting.py
@@ -8,4 +8,7 @@ class Meeting(db.Model):
     user2_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
     meeting_date = db.Column(db.Date)
     location = db.Column(db.String(100))
+    user1_comment = db.Column(db.Text)
+    user2_comment = db.Column(db.Text)
     result = db.Column(db.String(50))  # 'запланована', 'відбулася', 'відмова', 'успішна'
+    archived = db.Column(db.Boolean)
