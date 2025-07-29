54825fba5a09c21956a4fef43fd1b18459af13e5
Bananbka <bananbka0.2@gmail.com>
2025-07-29 15:24:26 +0300

    add weather api routes

---
+++ app/blueprints/api/weather.py
@@ -0,0 +1,32 @@
+﻿import os
+
+import requests
+from dotenv import load_dotenv
+from flask import Blueprint, request
+
+from app.utils.access_utils import api_error, login_required_api
+
+load_dotenv()
+
+weather_bp = Blueprint('weather', __name__)
+
+WEATHER_LINK = "https://api.openweathermap.org/data/2.5/weather"
+API_KEY = os.getenv("WEATHER_API_KEY")
+
+
+@weather_bp.route("/", methods=['GET'])
+@login_required_api
+def get_weather():
+    lat = request.args.get("lat")
+    lon = request.args.get("lon")
+
+    if not lat and not lon:
+        return api_error("Missing required parameters (lat, lon).", 400)
+
+    link = f"{WEATHER_LINK}?lat={lat}&lon={lon}&lang=ua&units=metric&appid={API_KEY}"
+    resp = requests.get(link)
+
+    if resp.status_code != 200:
+        return api_error("Error while getting weather", resp.status_code)
+
+    return resp.json()
