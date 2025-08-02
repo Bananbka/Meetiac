231761dc292ff072a0aa606f2d57f03f4ac65609
Bananbka <bananbka0.2@gmail.com>
2025-08-02 22:27:57 +0300

    update db with zodiac classes integration

---
+++ app/models/zodiac_compatibility.py
@@ -0,0 +1,13 @@
+﻿from app.database import db
+
+class ZodiacCompatibility(db.Model):
+    __tablename__ = 'zodiac_compatibility'
+
+    id = db.Column(db.Integer, primary_key=True)
+    sign1_id = db.Column(db.Integer, db.ForeignKey('zodiac_sign.sign_id'))
+    sign2_id = db.Column(db.Integer, db.ForeignKey('zodiac_sign.sign_id'))
+    percent = db.Column(db.Integer)
+    description = db.Column(db.Text)
+
+    sign1 = db.relationship('ZodiacSign', foreign_keys=[sign1_id])
+    sign2 = db.relationship('ZodiacSign', foreign_keys=[sign2_id])
---
+++ app/models/zodiac_prediction.py
@@ -0,0 +1,14 @@
+﻿from datetime import date
+
+from app.database import db
+
+class ZodiacPrediction(db.Model):
+    __tablename__ = 'zodiac_prediction'
+
+    id = db.Column(db.Integer, primary_key=True)
+    sign_id = db.Column(db.Integer, db.ForeignKey('zodiac_sign.sign_id'), unique=True)
+    prediction = db.Column(db.Text)
+    prediction_ua = db.Column(db.Text)
+    prediction_date = db.Column(db.Date, default=date.today)
+
+    sign = db.relationship('ZodiacSign', backref='predicted_zodiac_sign')
