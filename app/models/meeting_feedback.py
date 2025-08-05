ee2fe541312c181ffc559562e6cc02f0136d3b07
Bananbka <bananbka0.2@gmail.com>
2025-08-05 19:47:26 +0300

    add meeting feedback model

---
+++ app/models/meeting_feedback.py
@@ -0,0 +1,31 @@
+﻿from datetime import datetime, UTC
+
+from app.database import db
+
+
+class MeetingFeedback(db.Model):
+    __tablename__ = 'meeting_feedbacks'
+
+    feedback_id = db.Column(db.Integer, primary_key=True)
+    meeting_id = db.Column(db.Integer, db.ForeignKey('meetings.meeting_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    comment = db.Column(db.Text, nullable=False)
+    was_successful = db.Column(db.Boolean, nullable=False)
+    stay_together = db.Column(db.Boolean, default=False)
+    partner_late = db.Column(db.Boolean, default=False)
+
+    created_at = db.Column(db.DateTime, default=datetime.now(UTC))
+
+    meeting = db.relationship('Meeting', backref=db.backref('feedback', uselist=False, cascade='all, delete-orphan'))
+
+    def to_dict(self):
+        return {
+            'feedback_id': self.feedback_id,
+            'meeting_id': self.meeting_id,
+            'user_id': self.user_id,
+            'comment': self.comment,
+            'was_successful': self.was_successful,
+            'stay_together': self.stay_together,
+            'partner_late': self.partner_late,
+            'created_at': self.created_at
+        }
