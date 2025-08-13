d48bd51183a703010c623ea4c2d941df2ed9c1b6
Bananbka <bananbka0.2@gmail.com>
2025-08-13 15:30:20 +0300

    add restore code model

---
+++ app/models/restore_code.py
@@ -0,0 +1,12 @@
+﻿from app.database import db, default_expire
+
+
+class RestoreCode(db.Model):
+    __tablename__ = 'restore_code'
+
+    id = db.Column(db.Integer, primary_key=True)
+    creds_id = db.Column(db.Integer, db.ForeignKey('credentials.key_id'))
+    code = db.Column(db.String(6), nullable=False)
+    expire_at = db.Column(db.DateTime, nullable=False, default=default_expire)
+
+    creds = db.relationship('Credentials', backref='restore_code')
