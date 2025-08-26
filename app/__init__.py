8d40bad66180ef6ef5105313726b69b97d422c1f
Bananbka <bananbka0.2@gmail.com>
2025-08-26 18:34:48 +0300

    fix adaptive with header buttons | fix logging

--- app/__init__.py
+++ app/__init__.py
@@ -60,7 +60,7 @@ def create_app():
     if not os.path.exists("logs"):
         os.mkdir("logs")
 
-    file_handler = RotatingFileHandler("logs/app.log", maxBytes=10240, backupCount=5)
+    file_handler = RotatingFileHandler("logs/app.log", maxBytes=10240, backupCount=5, delay=True)
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(logging.Formatter(
         "%(asctime)s [%(levelname)s] %(message)s"
